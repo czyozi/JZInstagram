40012a8e0762bd07596b9f81fd07b3ae3b1b805e
czyozi <zhangchnz85@gmail.com>
2019-07-14 15:17:49 -0500

    CRUD finish

--- Insta/models.py
+++ Insta/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from imagekit.models import ProcessedImageField
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,3 +13,9 @@ class Post(models.Model):
         blank=True,
         null=True,
         )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
--- InstaDemo/urls.py
+++ InstaDemo/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Insta.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
