55c44e8f4b2de6489cec1c784a011cca5f9c3cdc
czyozi <zhangchnz85@gmail.com>
2019-07-13 19:16:16 -0500

    First Day

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from Insta.views import HelloDjango
+from Insta.views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', HelloDjango.as_view(), name='home'),
+    path('', PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
 
